5383d3fe498f5ee51eea4914e4266a91d38c9e8e
Sean Gillies <sean@mapbox.com>
2015-07-30 10:39:47 -0600

    Pre-release version, use of 0.25.0 platter.

--- mbtiles/__init__.py
+++ mbtiles/__init__.py
@@ -9,7 +9,7 @@ from rasterio._io import virtual_file_to_buffer
 
 buffer = bytes if sys.version_info > (3,) else buffer
 
-__version__ = '1.1.1'
+__version__ = '1.2a1'
 
 base_kwds = None
 src = None
