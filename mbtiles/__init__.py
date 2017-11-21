002e2717f81ae71910c38305048eb8dceee27cad
James McBride <jdmcbr@gmail.com>
2017-11-20 19:43:20 -0800

    Add comment explaining error filtering

--- mbtiles/__init__.py
+++ mbtiles/__init__.py
@@ -49,6 +49,8 @@ def process_tile(tile):
     src_nodata = kwds.pop('src_nodata', None)
     dst_nodata = kwds.pop('dst_nodata', None)
 
+    # set logger to filter warnings that appear due to a bug in rasterio;
+    # the bug will be fixed in rasterio 1.0, and then this can be removed
     gdal_log = logging.getLogger('rasterio._gdal')
     gdal_log_level = gdal_log.level
     gdal_log.setLevel(logging.CRITICAL)
