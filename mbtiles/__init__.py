97380b8d573b8997e9612d73801f1f3b2cb2638b
Sean C. Gillies <sean@mapbox.com>
2018-10-17 13:54:44 -0600

    Write out empty tiles when the reverse transform fails
    Resolves #41

--- mbtiles/__init__.py
+++ mbtiles/__init__.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 
 import mercantile
@@ -18,6 +19,8 @@ base_kwds = None
 src = None
 
 TILES_CRS = 'EPSG:3857'
+
+log = logging.getLogger(__name__)
 
 
 def init_worker(path, profile, resampling_method):
@@ -60,16 +63,21 @@ def process_tile(tile):
 
         # determine window of source raster corresponding to the tile
         # image, with small buffer at edges
-        west, south, east, north = transform_bounds(TILES_CRS, src.crs, ulx, lry, lrx, uly)
-        tile_window = window_from_bounds(west, south, east, north, transform=src.transform)
-        adjusted_tile_window = Window(
-            tile_window.col_off - 1, tile_window.row_off - 1,
-            tile_window.width + 2, tile_window.height + 2)
-        tile_window = adjusted_tile_window.round_offsets().round_shape()
+        try:
+            west, south, east, north = transform_bounds(TILES_CRS, src.crs, ulx, lry, lrx, uly)
+            tile_window = window_from_bounds(west, south, east, north, transform=src.transform)
+            adjusted_tile_window = Window(
+                tile_window.col_off - 1, tile_window.row_off - 1,
+                tile_window.width + 2, tile_window.height + 2)
+            tile_window = adjusted_tile_window.round_offsets().round_shape()
 
-        # if no data in window, skip processing the tile
-        if not src.read_masks(1, window=tile_window).any():
-            return tile, None
+        except ValueError:
+            log.info("Tile %r will not be skipped, even if empty. This is harmless.", tile)
+
+        else:
+            # if no data in window, skip processing the tile
+            if not src.read_masks(1, window=tile_window).any():
+                return tile, None
 
         reproject(rasterio.band(src, src.indexes),
                   rasterio.band(tmp, tmp.indexes),
--- tests/test_mod.py
+++ tests/test_mod.py
@@ -1,11 +1,16 @@
-import mercantile
+"""Module tests"""
+
+from mercantile import Tile
+import pytest
 import rasterio
 
 import mbtiles
 
 
-def test_process_tile(data):
-    mbtiles.init_worker(str(data.join('RGB.byte.tif')), {
+@pytest.mark.parametrize('tile', [Tile(36, 73, 7), Tile(0, 0, 0), Tile(1, 1, 1)])
+def test_process_tile(data, tile):
+    mbtiles.init_worker(
+        str(data.join('RGB.byte.tif')), {
             'driver': 'PNG',
             'dtype': 'uint8',
             'nodata': 0,
@@ -13,8 +18,8 @@ def test_process_tile(data):
             'width': 256,
             'count': 3,
             'crs': 'EPSG:3857'},
-            "nearest")
-    tile, contents = mbtiles.process_tile(mercantile.Tile(36, 73, 7))
-    assert tile.x == 36
-    assert tile.y == 73
-    assert tile.z == 7
+        'nearest')
+    t, contents = mbtiles.process_tile(tile)
+    assert t.x == tile.x
+    assert t.y == tile.y
+    assert t.z == tile.z
