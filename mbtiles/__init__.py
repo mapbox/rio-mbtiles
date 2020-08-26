f8e3095d2006d331a0da0931bee67a4cbe95db10
Sean Gillies <sean@mapbox.com>
2020-08-26 14:13:00 -0600

    Fix error in compat, move other warning back to mbtiles.__init__

--- mbtiles/__init__.py
+++ mbtiles/__init__.py
@@ -1,3 +1,13 @@
 """rio-mbtiles package"""
 
+import sys
+import warnings
+
 __version__ = "1.5dev"
+
+if sys.version_info < (3, 7):
+    warnings.warn(
+        "Support for Python versions < 3.7 will be dropped in rio-mbtiles version 2.0",
+        FutureWarning,
+        stacklevel=2,
+    )
--- mbtiles/compat.py
+++ mbtiles/compat.py
@@ -1,12 +1,7 @@
 import sys
 import warnings
 
-if sys.version_info < (3, 7):
-    warnings.warn(
-        "Support for Python versions < 3.7 will be dropped in rio-mbtiles version 2.0",
-        FutureWarning,
-        stacklevel=2,
-    )
+if sys.version_info < (3,):
     from itertools import izip_longest as zip_longest
 else:
     from itertools import zip_longest
